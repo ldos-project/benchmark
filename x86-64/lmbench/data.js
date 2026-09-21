d907dea92b1859ded7ef0ea2b21ee42ef85a086b
github-action-benchmark <github@users.noreply.github.com>
2026-09-21 19:43:24 +0000

    add process_getppid_lat (customSmallerIsBetter) benchmark result for f770cea880ee31088966b1de784b499aecb89fa1

--- x86-64/lmbench/data.js
+++ x86-64/lmbench/data.js
@@ -1,7 +1,47 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790019803904,
+  "lastUpdate": 1790019804607,
   "repoUrl": "https://github.com/ldos-project/asterinas",
   "entries": {
+    "process_getppid_lat": [
+      {
+        "commit": {
+          "author": {
+            "name": "gvipat",
+            "username": "gvipat",
+            "email": "gaurav.vipat@gmail.com"
+          },
+          "committer": {
+            "name": "gvipat",
+            "username": "gvipat",
+            "email": "gaurav.vipat@gmail.com"
+          },
+          "id": "f770cea880ee31088966b1de784b499aecb89fa1",
+          "message": "Fix act dependency fetch",
+          "timestamp": "2026-09-19T16:44:21Z",
+          "url": "https://github.com/ldos-project/asterinas/commit/f770cea880ee31088966b1de784b499aecb89fa1"
+        },
+        "date": 1790019804603,
+        "tool": "customSmallerIsBetter",
+        "title": "[Process] The cost of getppid",
+        "description": "lat_syscall null",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average syscall latency on Linux",
+            "value": "0.0515",
+            "unit": "µs",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average syscall latency on Asterinas",
+            "value": "0.0988",
+            "unit": "µs",
+            "extra": "aster_result"
+          }
+        ],
+        "releaseUrl": "https://github.com/ldos-project/benchmark/releases/tag/run-20260919-165421"
+      }
+    ],
     "process_shell_lat": [
       {
         "commit": {
