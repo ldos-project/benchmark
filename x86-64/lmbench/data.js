9c45c9d45e6451d0670d4f1eb71e1b77acb860b4
github-action-benchmark <github@users.noreply.github.com>
2026-09-21 19:43:28 +0000

    add mem_pagefault_lat (customSmallerIsBetter) benchmark result for f770cea880ee31088966b1de784b499aecb89fa1

--- x86-64/lmbench/data.js
+++ x86-64/lmbench/data.js
@@ -1,7 +1,47 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790019807495,
+  "lastUpdate": 1790019807931,
   "repoUrl": "https://github.com/ldos-project/asterinas",
   "entries": {
+    "mem_pagefault_lat": [
+      {
+        "commit": {
+          "author": {
+            "name": "gvipat",
+            "username": "gvipat",
+            "email": "gaurav.vipat@gmail.com"
+          },
+          "committer": {
+            "name": "gvipat",
+            "username": "gvipat",
+            "email": "gaurav.vipat@gmail.com"
+          },
+          "id": "f770cea880ee31088966b1de784b499aecb89fa1",
+          "message": "Fix act dependency fetch",
+          "timestamp": "2026-09-19T16:44:21Z",
+          "url": "https://github.com/ldos-project/asterinas/commit/f770cea880ee31088966b1de784b499aecb89fa1"
+        },
+        "date": 1790019807927,
+        "tool": "customSmallerIsBetter",
+        "title": "[Memory] The cost of page fault handling",
+        "description": "lat_pagefault",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average page fault latency on Linux",
+            "value": "0.0929",
+            "unit": "µs",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average page fault latency on Asterinas",
+            "value": "0.1351",
+            "unit": "µs",
+            "extra": "aster_result"
+          }
+        ],
+        "releaseUrl": "https://github.com/ldos-project/benchmark/releases/tag/run-20260919-165421"
+      }
+    ],
     "process_getppid_lat": [
       {
         "commit": {
