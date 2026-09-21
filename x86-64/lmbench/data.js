aa0d4df2952f254bd1e587358d0b237c9b320426
github-action-benchmark <github@users.noreply.github.com>
2026-09-21 19:43:20 +0000

    add tcp_loopback_bw_64k (customBiggerIsBetter) benchmark result for f770cea880ee31088966b1de784b499aecb89fa1

--- x86-64/lmbench/data.js
+++ x86-64/lmbench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790019799721,
+  "lastUpdate": 1790019800178,
   "repoUrl": "https://github.com/ldos-project/asterinas",
   "entries": {
     "unix_lat": [
@@ -396,6 +396,46 @@ window.BENCHMARK_DATA = {
             "name": "Average select file latency on Asterinas",
             "value": "2.4784",
             "unit": "µs",
+            "extra": "aster_result"
+          }
+        ],
+        "releaseUrl": "https://github.com/ldos-project/benchmark/releases/tag/run-20260919-165421"
+      }
+    ],
+    "tcp_loopback_bw_64k": [
+      {
+        "commit": {
+          "author": {
+            "name": "gvipat",
+            "username": "gvipat",
+            "email": "gaurav.vipat@gmail.com"
+          },
+          "committer": {
+            "name": "gvipat",
+            "username": "gvipat",
+            "email": "gaurav.vipat@gmail.com"
+          },
+          "id": "f770cea880ee31088966b1de784b499aecb89fa1",
+          "message": "Fix act dependency fetch",
+          "timestamp": "2026-09-19T16:44:21Z",
+          "url": "https://github.com/ldos-project/asterinas/commit/f770cea880ee31088966b1de784b499aecb89fa1"
+        },
+        "date": 1790019800175,
+        "tool": "customBiggerIsBetter",
+        "title": "[TCP sockets] The bandwidth (loopback, 64KB message)",
+        "description": "bw_tcp -l",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average TCP bandwidth on Linux",
+            "value": "7398.07",
+            "unit": "MB/s",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average TCP bandwidth on Asterinas",
+            "value": "7436.62",
+            "unit": "MB/s",
             "extra": "aster_result"
           }
         ],
