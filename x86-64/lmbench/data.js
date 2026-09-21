5b2e591404a421d0bc59443113569a0264cdfe8c
github-action-benchmark <github@users.noreply.github.com>
2026-09-21 19:43:13 +0000

    add vfs_read_lat (customSmallerIsBetter) benchmark result for f770cea880ee31088966b1de784b499aecb89fa1

--- x86-64/lmbench/data.js
+++ x86-64/lmbench/data.js
@@ -1,7 +1,47 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790019793006,
+  "lastUpdate": 1790019793393,
   "repoUrl": "https://github.com/ldos-project/asterinas",
   "entries": {
+    "vfs_read_lat": [
+      {
+        "commit": {
+          "author": {
+            "name": "gvipat",
+            "username": "gvipat",
+            "email": "gaurav.vipat@gmail.com"
+          },
+          "committer": {
+            "name": "gvipat",
+            "username": "gvipat",
+            "email": "gaurav.vipat@gmail.com"
+          },
+          "id": "f770cea880ee31088966b1de784b499aecb89fa1",
+          "message": "Fix act dependency fetch",
+          "timestamp": "2026-09-19T16:44:21Z",
+          "url": "https://github.com/ldos-project/asterinas/commit/f770cea880ee31088966b1de784b499aecb89fa1"
+        },
+        "date": 1790019793390,
+        "tool": "customSmallerIsBetter",
+        "title": "[VFS] The cost of read (/dev/zero)",
+        "description": "lat_syscall read",
+        "display": true,
+        "benches": [
+          {
+            "name": "Average read latency on Linux",
+            "value": "0.0858",
+            "unit": "µs",
+            "extra": "linux_result"
+          },
+          {
+            "name": "Average read latency on Asterinas",
+            "value": "0.1426",
+            "unit": "µs",
+            "extra": "aster_result"
+          }
+        ],
+        "releaseUrl": "https://github.com/ldos-project/benchmark/releases/tag/run-20260919-165421"
+      }
+    ],
     "vfs_write_lat": [
       {
         "commit": {
